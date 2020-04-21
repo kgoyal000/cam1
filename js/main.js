2d69e93973a2e88efd034af3741e672219655d58
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-04-21 16:50:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
         'Please use Chrome, Firefox, iOS 11, Android 5 or higher, Safari 11 or higher',
       );
     }else {
-//       if (DetectRTC.hasWebcam == false) {
-//         alert('Please install an external webcam device.');
-//       } else {
+      if (DetectRTC.hasWebcam == false) {
+        alert('Please install an external webcam device.');
+      } else {
         amountOfCameras = DetectRTC.videoInputDevices.length;
 
         initCameraUI();
